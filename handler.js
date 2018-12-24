9131b1d219bda128279dbbb27f81d2e94344a090
awitherow <au.witherow@gmail.com>
2018-12-24 13:09:02 -1000

    update email used

--- handler.js
+++ handler.js
@@ -6,98 +6,90 @@ const AWS = require('aws-sdk');
 const SES = new AWS.SES();
 
 function sendMail(formData, cb) {
-  let emailParams = {};
+	let emailParams = {};
 
-  if (formData['cart']) {
-    const {
-      from,
-      contact,
-      addrOne,
-      addrTwo,
-      city,
-      state,
-      zip,
-      tel,
-      body,
-      cart,
-      frequency,
-      payment,
-      source
-    } = formData;
+	if (formData['cart']) {
+		const {
+			from,
+			contact,
+			addrOne,
+			addrTwo,
+			city,
+			state,
+			zip,
+			tel,
+			body,
+			cart,
+			frequency,
+			payment,
+			source
+		} = formData;
 
-    const orderList = cart.map(({
-      name,
-      value
-    }) => `- ${value} ${name}\n`);
+		const orderList = cart.map(({ name, value }) => `- ${value} ${name}\n`);
 
-    var addrTwoExists = addrTwo ? `, ${addrTwo}` : '';
+		var addrTwoExists = addrTwo ? `, ${addrTwo}` : '';
 
-    emailParams = {
-      Source: 'mailgun@awitherow.com',
-      ReplyToAddresses: [from],
-      Destination: {
-        ToAddresses: ['aloha@farmily.ventures']
-      },
-      Message: {
-        Body: {
-          Text: {
-            Charset: 'UTF-8',
-            Data: `Contact: ${contact}\nEmail: ${from}\nPhone: ${tel}\nAddress: ${addrOne}${addrTwoExists}, ${city}, ${state}, ${zip}\nOrder:\n${orderList}\nSource: ${source}\nFrequency: ${frequency}\nPayment Method: ${payment}\nDetails: ${body}`
-          }
-        },
-        Subject: {
-          Charset: 'UTF-8',
-          Data: `Veggie Box Order | ${contact} - ${zip}`
-        }
-      }
-    };
-  } else {
-    const {
-      contact,
-      from,
-      subject,
-      body
-    } = formData;
+		emailParams = {
+			Source: 'mailgun@awitherow.com',
+			ReplyToAddresses: [ from ],
+			Destination: {
+				ToAddresses: [ 'aloha@alohawai.farm' ]
+			},
+			Message: {
+				Body: {
+					Text: {
+						Charset: 'UTF-8',
+						Data: `Contact: ${contact}\nEmail: ${from}\nPhone: ${tel}\nAddress: ${addrOne}${addrTwoExists}, ${city}, ${state}, ${zip}\nOrder:\n${orderList}\nSource: ${source}\nFrequency: ${frequency}\nPayment Method: ${payment}\nDetails: ${body}`
+					}
+				},
+				Subject: {
+					Charset: 'UTF-8',
+					Data: `Veggie Box Order | ${contact} - ${zip}`
+				}
+			}
+		};
+	} else {
+		const { contact, from, subject, body } = formData;
 
-    emailParams = {
-      Source: 'mailgun@awitherow.com',
-      ReplyToAddresses: [from],
-      Destination: {
-        ToAddresses: ['aloha@farmily.ventures'] // SES RECEIVING EMAIL
-      },
-      Message: {
-        Body: {
-          Text: {
-            Charset: 'UTF-8',
-            Data: `Contact: ${contact}\nEmail: ${from}\n\n ${body}`
-          }
-        },
-        Subject: {
-          Charset: 'UTF-8',
-          Data: `INQUIRY | [${contact}] - ${subject}`
-        }
-      }
-    };
-  }
+		emailParams = {
+			Source: 'mailgun@awitherow.com',
+			ReplyToAddresses: [ from ],
+			Destination: {
+				ToAddresses: [ 'aloha@alohawai.farm' ] // SES RECEIVING EMAIL
+			},
+			Message: {
+				Body: {
+					Text: {
+						Charset: 'UTF-8',
+						Data: `Contact: ${contact}\nEmail: ${from}\n\n ${body}`
+					}
+				},
+				Subject: {
+					Charset: 'UTF-8',
+					Data: `INQUIRY | [${contact}] - ${subject}`
+				}
+			}
+		};
+	}
 
-  SES.sendEmail(emailParams, cb);
+	SES.sendEmail(emailParams, cb);
 }
 
 module.exports.mail = (event, context, callback) => {
-  const formData = JSON.parse(event.body);
+	const formData = JSON.parse(event.body);
 
-  sendMail(formData, function (err, data) {
-    const response = {
-      statusCode: err ? 500 : 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'https://farmily.ventures'
-      },
-      body: JSON.stringify({
-        message: err ? err.message : data
-      })
-    };
+	sendMail(formData, function(err, data) {
+		const response = {
+			statusCode: err ? 500 : 200,
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': 'https://alohawai.farm'
+			},
+			body: JSON.stringify({
+				message: err ? err.message : data
+			})
+		};
 
-    callback(null, response);
-  });
+		callback(null, response);
+	});
 };
