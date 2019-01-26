cc5c9b486fe26e2db80b8e6fd4a2f7b50ba0d9d6
awitherow <au.witherow@gmail.com>
2019-01-25 17:12:27 -1000

    forgot to remove zip

--- handler.js
+++ handler.js
@@ -12,63 +12,66 @@ function sendMail(formData, cb) {
 		const {
 			from,
 			contact,
-			addrOne,
-			addrTwo,
-			city,
-			state,
-			zip,
+			address,
 			tel,
 			body,
 			cart,
 			frequency,
 			payment,
-			source
+			source,
+			price,
 		} = formData;
 
 		const orderList = cart.map(({ name, value }) => `- ${value} ${name}\n`);
 
-		var addrTwoExists = addrTwo ? `, ${addrTwo}` : '';
-
 		emailParams = {
 			Source: 'mailgun@awitherow.com',
-			ReplyToAddresses: [ from ],
+			ReplyToAddresses: [
+				from,
+			],
 			Destination: {
-				ToAddresses: [ 'aloha@alohawai.farm' ]
+				ToAddresses: [
+					'aloha@alohawai.farm',
+				],
 			},
 			Message: {
 				Body: {
 					Text: {
 						Charset: 'UTF-8',
-						Data: `Contact: ${contact}\nEmail: ${from}\nPhone: ${tel}\nAddress: ${addrOne}${addrTwoExists}, ${city}, ${state}, ${zip}\nOrder:\n${orderList}\nSource: ${source}\nFrequency: ${frequency}\nPayment Method: ${payment}\nDetails: ${body}`
-					}
+						Data: `Contact: ${contact}\nEmail: ${from}\nPhone: ${tel}\nAddress: ${address}\nOrder:\n${orderList}\nSource: ${source}\nFrequency: ${frequency}\nPayment Method: ${payment}\nCost: ${price}\nDetails: ${body}`,
+					},
 				},
 				Subject: {
 					Charset: 'UTF-8',
-					Data: `Veggie Box Order | ${contact} - ${zip}`
-				}
-			}
+					Data: `Veggie Box Order | ${contact} - $${price}`,
+				},
+			},
 		};
 	} else {
 		const { contact, from, subject, body } = formData;
 
 		emailParams = {
 			Source: 'mailgun@awitherow.com',
-			ReplyToAddresses: [ from ],
+			ReplyToAddresses: [
+				from,
+			],
 			Destination: {
-				ToAddresses: [ 'aloha@alohawai.farm' ] // SES RECEIVING EMAIL
+				ToAddresses: [
+					'aloha@alohawai.farm',
+				], // SES RECEIVING EMAIL
 			},
 			Message: {
 				Body: {
 					Text: {
 						Charset: 'UTF-8',
-						Data: `Contact: ${contact}\nEmail: ${from}\n\n ${body}`
-					}
+						Data: `Contact: ${contact}\nEmail: ${from}\n\n ${body}`,
+					},
 				},
 				Subject: {
 					Charset: 'UTF-8',
-					Data: `INQUIRY | [${contact}] - ${subject}`
-				}
-			}
+					Data: `INQUIRY | [${contact}] - ${subject}`,
+				},
+			},
 		};
 	}
 
@@ -83,11 +86,11 @@ module.exports.mail = (event, context, callback) => {
 			statusCode: err ? 500 : 200,
 			headers: {
 				'Content-Type': 'application/json',
-				'Access-Control-Allow-Origin': 'https://alohawai.farm'
+				'Access-Control-Allow-Origin': 'https://alohawai.farm',
 			},
 			body: JSON.stringify({
-				message: err ? err.message : data
-			})
+				message: err ? err.message : data,
+			}),
 		};
 
 		callback(null, response);
